6782b5407296cd06e3eb9d09a8c5183ee79750cb
Devin Bolden <devinbolden6@gmail.com>
2022-12-23 08:54:21 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './less/index.less'
 
 // Your code goes here!
+// EventNumber 1 (not complete)
+const docBody = document.querySelector('body')
+docBody.style.background = 'darkgray'
+// console.log(docBody)
+
+// EventNumber 2 (not complete)
+const busPic = document.querySelector('header')
+// console.log(busPic)
+function onClick(){
+    docBody.style.background('orange')
+}
+    busPic.addEventListener('click', onClick)
+
+// EventNumber 3 (not compolete)
+const home = document.querySelector('header nav a:nth-of-type(1)')    
+
+
+// EventNumber 4 (not complete)
+const contentDestination = document.querySelector('.content-destination')
+console.log(contentDestination)
